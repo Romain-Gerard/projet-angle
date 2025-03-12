17a31e230e7b44d0def955924ebab977a710d356
gaelleloutfi <150391484+gaelleloutfi@users.noreply.github.com>
2025-03-12 12:51:12 +0100

    Update frame_extraction.py

--- scripts/frame_extraction.py
+++ scripts/frame_extraction.py
@@ -1,1 +1,14 @@
 import MDAnalysis as mda
+
+#we will load the trajectory using start.gro as our topology file
+u=mda.Universe("../data/start.gro", "../data/md_OK_dt100.xtc")
+
+#now we will create .gro files for each frame in the xtc
+for ts in u.trajectory:
+  frame_number = ts.frame #getting the frame index
+  output_filename=f"frame_{frame_number:04d}.gro" #eg: frame_0001.gro
+
+  with mda.Writer(output_filename, n_atoms = u.atoms.n_atoms) as w:
+    w.write(u.atoms)
+
+print(f"Saved: {output_filename}")
