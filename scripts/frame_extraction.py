fddf322196661ef2242b44aad9462f441b429d02
gaelleloutfi <150391484+gaelleloutfi@users.noreply.github.com>
2025-03-13 13:21:24 +0100

    Update frame_extraction.py

--- scripts/frame_extraction.py
+++ scripts/frame_extraction.py
@@ -1,16 +1,20 @@
 import MDAnalysis as mda
+import os
 
 # Charger la trajectoire en utilisant start.gro comme fichier de topologie
 u = mda.Universe("../data/start.gro", "../data/md_OK_dt100.xtc")
 
-# Parcourir toutes les frames de la trajectoire
+# Définition du dossier de sortie
+output_folder = "../output/frames"
+os.makedirs(output_folder, exist_ok=True)  # Créer le dossier s'il n'existe pas
+
+# Parcourir toutes les frame de la trajectoire
 for ts in u.trajectory:
     frame_number = ts.frame  # Récupérer l'index de la frame actuelle
-    output_filename = f"frame_{frame_number:04d}.gro"  # Générer un nom de fichier, ex : frame_0001.gro
+    output_filename = os.path.join(output_folder, f"frame_{frame_number:04d}.gro")  # Chemin complet du fichier
 
     # Écrire la frame actuelle dans un fichier .gro
     with mda.Writer(output_filename, n_atoms=u.atoms.n_atoms) as w:
         w.write(u.atoms)  # Sauvegarder les atomes de la frame actuelle
 
-# Afficher le dernier fichier sauvegardé
-print(f"Sauvegardé : {output_filename}")
+print(f"Frames sauvegardées dans : {output_folder}")
